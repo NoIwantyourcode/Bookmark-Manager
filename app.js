046b998721bbed39573ea682bfd6fb70dd29c90b
Naytik <121176288+NoIwantyourcode@users.noreply.github.com>
2026-05-05 18:52:28 +0800

    added the ability to fetch the favicon and title as well as URL of the page

--- app.js
+++ app.js
@@ -2,28 +2,45 @@ let bookmarks = JSON.parse(localStorage.getItem('bookmarks') || '[]');
 
 function renderBookmarks() {
     const list = document.getElementById('bookmarkList');
-    list.innerHTML = '';
 
     bookmarks.forEach(bookmark => {
         const item = document.createElement('div');
         item.classList.add('bookmark');
+        list.innerHTML = `
+            <img src="${bookmark.favicon}" width="16" height="16">
+            <a href="${bookmark.url}" target="_blank">${bookmarks.title}</a>
+        `;
         item.textContent = bookmark.url;
         list.appendChild(item);
     });
 }
 
-document.getElementById('addBtn').addEventListener('click', () => {
-    const url = document.getElementById('urlInput').ariaValueMax.trim();
+document.getElementById('addBtn').addEventListener('click', async () => {
+    const url = document.getElementById('urlInput').value.trim();
     if (!url) return;
+
+    const {title, favicon} = await fetchPageInfo(url);
+
     bookmarks.push({
-        id: Date.now(), url,
-        title: url,
-        tags: [],
-    });
+        id: Date.now(), url, title, favicon,
+        tags: []
+    }); 
 
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     document.getElementById('urlInput').value = '';
     renderBookmarks();
 })
+
+async function fetchPageInfo(url) {
+    try {
+        const hostname = new URL(url).hostname;
+        const favicon = `https://www.google.com/s2/favicons?domain=${hostname}&sz=32`
+        console.log(favicon);
+        return {title: hostname, favicon}
+    } catch (e) {
+        console.log('error: ', e)
+        return { title: url, favicon: ''};
+    }
+}
 
 renderBookmarks();
